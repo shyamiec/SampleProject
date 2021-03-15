7a63c9d51705d470a96a197590a75bbde61a7f3a
Shyam AGRAHARI <shyam.agrahari@soprasteria.com>
2021-03-15 12:26:17 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
